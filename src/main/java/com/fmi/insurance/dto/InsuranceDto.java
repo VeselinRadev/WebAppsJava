b9f3032f016ea8ccfb57c7dfed75a5bb912ba15b
denisbilyal <denisbilyal@gmail.com>
2025-06-21 17:45:19 +0300

    Add new atributes Insurance Model

--- src/main/java/com/fmi/insurance/dto/InsuranceDto.java
+++ src/main/java/com/fmi/insurance/dto/InsuranceDto.java
@@ -3,18 +3,20 @@ package com.fmi.insurance.dto;
 import com.fmi.insurance.model.Insurance;
 
 public record InsuranceDto (
-    Long id,
     String policyNumber,
     String startDate,
     String endDate,
+    String sticker,
+    String greenCard,
     String details
 ){
     public static InsuranceDto fromEntity(Insurance insurance) {
         return new InsuranceDto(
-            insurance.getId(),
             insurance.getPolicyNumber(),
             insurance.getStartDate().toString(),
             insurance.getEndDate().toString(),
+            insurance.getSticker(),
+            insurance.getGreenCard(),
             insurance.getDetails() != null ? insurance.getDetails() : ""
         );
     }
--- src/main/java/com/fmi/insurance/model/Insurance.java
+++ src/main/java/com/fmi/insurance/model/Insurance.java
@@ -6,8 +6,6 @@ import java.util.Set;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -39,6 +37,14 @@ public class Insurance {
     @Column(nullable = false)
     @NotNull
     private Date endDate;
+
+    @Column(nullable = false, unique = true)
+    @NotNull
+    private String sticker;
+
+    @Column(nullable = false, unique = true)
+    @NotNull
+    private String greenCard;
 
     @Column
     private String details;
