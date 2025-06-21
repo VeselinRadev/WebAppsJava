b0e4eca4317a7c195ec3211a7eea5b49303f305f
denisbilyal <denisbilyal@gmail.com>
2025-06-21 10:50:45 +0300

    Remove unused imports

--- src/main/java/com/fmi/insurance/model/Client.java
+++ src/main/java/com/fmi/insurance/model/Client.java
@@ -5,8 +5,6 @@ import java.util.Set;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
