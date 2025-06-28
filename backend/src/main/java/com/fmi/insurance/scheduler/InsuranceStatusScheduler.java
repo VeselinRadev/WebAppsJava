874c2cf42e7cfc07a6c20d460b63e9c770899b11
eramadanova <elisramadanova2003@gmail.com>
2025-06-28 11:35:18 +0300

    Fix Scheduler

--- backend/src/main/java/com/fmi/insurance/scheduler/InsuranceStatusScheduler.java
+++ backend/src/main/java/com/fmi/insurance/scheduler/InsuranceStatusScheduler.java
@@ -6,7 +6,6 @@ import com.fmi.insurance.model.Insurance;
 import com.fmi.insurance.model.Payment;
 import com.fmi.insurance.repository.InsuranceRepository;
 import com.fmi.insurance.repository.PaymentRepository;
-import com.fmi.insurance.repository.custom.PaymentRepositoryCustom;
 import com.fmi.insurance.service.CarService;
 import com.fmi.insurance.service.InsuranceService;
 import com.fmi.insurance.vo.InsuranceStatus;
@@ -23,7 +22,6 @@ import java.util.List;
 public class InsuranceStatusScheduler {
     private final InsuranceRepository insuranceRepository;
     private final PaymentRepository paymentRepository;
-    private final CarService carService;
     private final InsuranceService insuranceService;
 
     @Scheduled(cron = "0 0 0 * * ?")
@@ -44,7 +42,7 @@ public class InsuranceStatusScheduler {
     public void checkPayments() {
         LocalDate today = LocalDate.now().plusDays(15);
         PaymentSearchParamDto searchParams = PaymentSearchParamDto.builder()
-                .afterDate(Date.valueOf(today))
+                .beforeDate(Date.valueOf(today))
                 .build();
 
         paymentRepository.findPaymentsByCriteria(searchParams).stream()
